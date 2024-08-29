a1f0838a0c85b7f6c282765c874482e954f3fba0
Simon <clayto23@lancaster.ac.uk>
2024-08-29 21:12:09 +0100

    Coded BattleshipTile class

--- src/BattleshipTile.java
+++ src/BattleshipTile.java
@@ -1,2 +1,12 @@
-public class BattleshipTile {
+import javax.swing.*;
+
+public class BattleshipTile extends JButton {
+    boolean isSunk;
+
+    public BattleshipTile() {
+        isSunk = false;
+    }
+    public boolean isSunk() {
+        return isSunk;
+    }
 }
